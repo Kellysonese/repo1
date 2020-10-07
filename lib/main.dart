2bb5e5d9bd806c5738f0406d84bd3012c5fa42ad
FlutLab.io <root@vmi348945.contaboserver.net>
2020-10-07 16:24:00 +0200

    ch4

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ adsasd
 d
 adsasddsa
 
+hh  dfd fd fdf
+GestureRecognizerFactoryWithHandlers<T:Object>gsdf
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
